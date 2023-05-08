3d97e2874ff0aab7ff3ad8c499d7cea58757c00b
Perry Fardella <perry.fardella@live.com>
2023-05-08 21:59:52 +0800

    Add add to calendar button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import { AddToCalendarButton } from "add-to-calendar-button-react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <AddToCalendarButton
+          name="Title"
+          options={["Apple", "Google"]}
+          location="World Wide Web"
+          startDate="2023-05-11"
+          endDate="2023-05-11"
+          startTime="10:15"
+          endTime="23:30"
+          timeZone="America/Los_Angeles"
+        />
+      </div>
     </main>
   );
 }
